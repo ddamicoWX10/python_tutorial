8201eb6e35d625fd8b6fefed090fa469e04b439a
Dan D'Amico <damico@vpn65-37.vpnclient.ucar.edu>
2020-10-28 13:36:11 -0600

    Parsing file

--- mysci.py
+++ mysci.py
@@ -1,12 +1,25 @@
 # Comment in python is #
-# Reading a data file
+# Initialize data variable
+data = []
 
+# Reading a data file
 filename = "data/wxobs20170821.txt"
 
 with open(filename,'r') as datafile:
-    data = datafile.read()
+    # Read first three lines (header)
+    for _ in range(3):
+        datafile.readline()
+
+    # Read and parse the rest of the file
+    for line in datafile:
+        datum = line.split()
+        data.append(datum)
+
+# In square brackets, it would essentially be [start:stop:step]
+# If nesting, need to have [x][y][z], not[x[y[z]]]
 # Python is more sensitive to whitespace (dumb, why?)
 # Careful with indentation
 # Best practice for reading is with the "with" command
+
 
 

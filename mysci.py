d982ae0e0758b38ffba9695bec17a01866478129
Dan D'Amico <damico@vpn65-37.vpnclient.ucar.edu>
2020-10-28 13:47:37 -0600

    Converting tempout to float

--- mysci.py
+++ mysci.py
@@ -17,7 +17,7 @@ with open(filename,'r') as datafile:
         split_line = line.split()
         data['date'].append(split_line[0])
         data['time'].append(split_line[1])
-        data['tempout'].append(split_line[2])
+        data['tempout'].append(float(split_line[2]))
 
 
 # In square brackets, it would essentially be [start:stop:step]
